4e9787019c49c283fddec7a28b0fef7d28e5b57b
NishanthGSuryavamshi <catchnishugs@gmail.com>
2023-04-09 17:00:00 +0530

    data ingestion

--- src/components/data_ingestion2.py
+++ src/components/data_ingestion2.py
@@ -1,19 +1,17 @@
-import os
 import sys
-from src.exception import CustomException
+import os
 from src.logger import logging
-
+from sklearn.model_selection import train_test_split
+from src.exception import CustomException
 import pandas as pd
 from dataclasses import dataclass
-
-from sklearn.model_selection import train_test_split
 
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifacts', "train.csv")
-    test_data_path: str = os.path.join('artifacts', "test.csv")
-    raw_data_path: str = os.path.join('artifacts', "raw.csv")
+    train_data_path: str = os.path.join('artifacts_2', 'train_1.csv')
+    test_data_path: str = os.path.join('artifacts_2', 'test_.csv')
+    raw_data_path: str = os.path.join('artifacts_2', 'raw_.csv')
 
 
 class DataIngestion:
@@ -21,23 +19,25 @@ class DataIngestion:
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("entered the data ingestion component")
+
+        logging.info('data ingestion initiated')
         try:
             df = pd.read_csv('Notebook\Data\StudentsPerformance.csv')
-            logging.info('read the dataset as dataframe')
+            logging.info('read the data as dataframe')
+
             os.makedirs(os.path.dirname(
                 self.ingestion_config.train_data_path), exist_ok=True)
             df.to_csv(self.ingestion_config.train_data_path)
-            logging.info('train test split initiated')
+            logging.info('train_test_split initiated')
+
             train_set, test_set = train_test_split(
                 df, test_size=.2, random_state=42)
             train_set.to_csv(self.ingestion_config.train_data_path)
             test_set.to_csv(self.ingestion_config.test_data_path)
-            logging.info('ingestion of the data is completed')
+            logging.info('train and test dataset saved in artifacts folder')
             return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path,
-
             )
 
         except Exception as e:
--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -21,11 +21,3 @@ class CustomException(Exception):
     def __str__(self):
 
         return self.error_message
-
-
-if __name__ == '__main__':
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info('divided by 0')
-        raise CustomException(e, sys)
